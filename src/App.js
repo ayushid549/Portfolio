b7b85bfd1436fbc9984394de1fd339d38a7f07e8
ayushid549 <ayushid549@gmail.com>
2022-11-05 21:31:46 +0530

    routes changed

--- src/App.js
+++ src/App.js
@@ -5,26 +5,25 @@ import AboutMe from './components/AboutMe';
 import Education from './components/Education';
 import SkillComp from './components/SkillComp';
 import CodingCard from './components/CodingCard';
-//import CodingProfile from './components/CodingProfile';
 import Contact from './components/Contact';
-import Footer from './components/Footer.js';
 import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-      <Routes>
+    <div>
+
+    <Routes>
+        
+          
         <Route exact={true} path='/' element={<Homepage/>} />
         <Route  path="/About" element={<AboutMe/>} />
         <Route  path="/Education" element={<Education/>} />
         <Route  path="/CodingCard" element={<CodingCard/>} />
         <Route  path="/SkillComp" element={<SkillComp/>} />
         <Route  path="/Contact" element={<Contact/>} />
-        <Route  path="/Footer" element={<Footer/>} />
       </Routes>
-    
-      
-    </>
+
+    </div>
   );
 }
 export default App;
